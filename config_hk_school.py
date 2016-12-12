22ac58f9e0a1244ca18b14675aefff393f069546
Daedalus Chan <daedalus.chan@gmail.com>
2016-12-12 23:29:55 +0800

    updated the base URL to 2016

--- config_hk_school.py
+++ config_hk_school.py
@@ -2,8 +2,11 @@
 # In general these URL need not to be changed. Except when ever on a new school year,
 # then the domain name will be changed accordingly.
 ###############################################################################################
-school_list_url = 'http://kgp2015.highlight.hk/website/school.php'
-school_info_url = 'http://kgp2015.highlight.hk/website/schoolinfo.php?schid='
+# school_list_url = 'http://kgp2015.highlight.hk/website/school.php'
+# school_info_url = 'http://kgp2015.highlight.hk/website/schoolinfo.php?schid='
+
+school_list_url = 'http://kgp2016.highlight.hk/web/school.php'
+school_info_url = 'http://kgp2016.highlight.hk/web/schoolinfo.php?schid='
 
 ###############################################################################################
 # input the proxy host / port in the format of:
